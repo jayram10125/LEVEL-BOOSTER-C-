af36258901d5481127c203a61067cba66b4b2fa0
jayram10125 <jayram.10125@gmail.com>
2024-03-17 22:54:55 +0530

    comments

---
+++ BASICS OF CPP/comments.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    //cout<<"hello jay"<<endl;    // double forward slash is used to single line comment
+    cout<<"hello jay"<<endl;
+    /*cout<<"hello jay"<<endl;
+    cout<<"hello jay"<<endl;
+    cout<<"hello jay"<<endl;*/    /*for multi line comments we use forward slash and asterisk and comment and the asterisk forward slash*/
+    cout<<"hello jay"<<endl;
+    cout<<"hello jay"<<endl;
+    return 0;
+}
