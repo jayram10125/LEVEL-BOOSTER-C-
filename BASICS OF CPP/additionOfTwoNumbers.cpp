68f35f3ad2806646c172b761cf9f589c70b492eb
jayram10125 <jayram.10125@gmail.com>
2024-03-17 22:07:36 +0530

    addition of two numbers

---
+++ BASICS OF CPP/additionOfTwoNumbers.cpp
@@ -0,0 +1,11 @@
+//adding two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter two numbers : ";
+    cin>>a>>b;
+    cout<<"addition of "<<a<<" and "<<b<<" is : "<<a+b;
+    return 0;
+}
