2559af227ce324677ec2adee4b2380306b288297
jayram10125 <jayram.10125@gmail.com>
2024-03-17 22:20:33 +0530

    clear the terminal method 2

---
+++ BASICS OF CPP/clrTerminalM2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#define clrscr() system("cls"); //defining the function clrscr() as system("cls")
+using namespace std;
+int main()
+{
+    clrscr();
+    cout<<"welcome!!";
+    clrscr();
+    return 0;
+}
