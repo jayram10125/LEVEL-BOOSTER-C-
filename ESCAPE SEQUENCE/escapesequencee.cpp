3bb00d8c269899ec32e26be5dbf9e2a230d0ebe5
jayram10125 <jayram.10125@gmail.com>
2024-03-17 22:43:35 +0530

    escape sequence

---
+++ ESCAPE SEQUENCE/escapesequencee.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello \nworld"<<endl; //    \n ---->for new line Also endl is used to next line but difference is that \n should be in double iinverted commas but there is no need of inverted commas for endl
+    cout<<"hello \tworld"<<endl;//     \t ---->fot a tab space i.e 7 space
+    cout<<"\"HELLOW\""<<endl; //  \" to get the output in inverted commas
+    cout<<"\'welcome\'"<<endl;//  \' to get the output in single inverted commas
+    cout<<"    jay"<<endl; 
+    cout<<" \rjay"<<endl; //to remove the previous space 
+    cout<<"\a"<<endl; // to create an alarm beep but depends on system
+    return 0;
+}
+
