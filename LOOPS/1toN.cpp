0134d4676230788aa6d7351d0eddcc3567b7609c
jayram10125 <jayram.10125@gmail.com>
2024-03-27 23:55:16 +0530

    main

---
+++ LOOPS/1toN.cpp
@@ -0,0 +1,14 @@
+//printing 1 to N
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter limit : ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<i<<" ";
+    }
+    return 0;
+}
