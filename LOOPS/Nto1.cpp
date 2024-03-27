19f4d283db4beb61b8c4f53a540bddbdcd7e4912
jayram10125 <jayram.10125@gmail.com>
2024-03-27 23:58:50 +0530

    N to 1

---
+++ LOOPS/Nto1.cpp
@@ -0,0 +1,14 @@
+//printing N to 1
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter limit : ";
+    cin>>n;
+    for(int i=n;i>=1;i--)
+    {
+        cout<<i<<" ";
+    }
+    return 0;
+}
