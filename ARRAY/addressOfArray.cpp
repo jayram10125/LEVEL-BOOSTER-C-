8aa121dcecf93d607314a41a828c960d8f9733ae
jayram10125 <jayram.10125@gmail.com>
2024-03-30 22:36:51 +0530

    address of array

---
+++ ARRAY/addressOfArray.cpp
@@ -0,0 +1,14 @@
+//printiing address of array
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={2,3,4,5,6};
+    cout<<&arr[0]<<endl;
+    cout<<&arr[1]<<endl;
+    cout<<&arr[2]<<endl;
+    cout<<&arr[3]<<endl;
+    cout<<&arr[4]<<endl;
+    return 0;
+
+}
