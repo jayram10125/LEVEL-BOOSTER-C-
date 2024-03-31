a2a1ffe82b0f71a21986f9324c7a17616e0593ca
jayram10125 <jayram.10125@gmail.com>
2024-03-31 21:26:56 +0530

    minimum element of array

---
+++ ARRAY/minimumElementOfArray.cpp
@@ -0,0 +1,25 @@
+//find the maximum element of the array 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,min;
+    cout<<"enter the size of array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"enter the elements of array : ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    min=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]<min)
+        {
+            min=arr[i];
+        }
+    }
+    cout<<"minimum element is : "<<min;
+    return 0;
+}
