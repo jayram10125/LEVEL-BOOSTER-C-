f44ae04b5d82f33de1cd0a67763df445fb057822
jayram10125 <jayram.10125@gmail.com>
2024-04-03 10:02:42 +0530

    prime or not

---
+++ LOOPS/primeNumber.cpp
@@ -0,0 +1,21 @@
+// check the given number is prime or not
+#include<iostream>
+using namespace std;
+int main()
+{ int n,a=0;
+cout<<"enter a number : ";
+cin>>n;
+for(int i=2;i<=n-1;i++)
+{
+    if(n%i==0)
+    {
+        a=1;
+    }
+}
+if(a==0)
+cout<<"this is a prime number ";
+else
+cout<<"this is not a prime number : ";
+    
+    return 0;
+}
