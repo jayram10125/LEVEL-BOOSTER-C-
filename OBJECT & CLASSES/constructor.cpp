1f6aee78e17cac531a8b9e8fbdd76bd74f7f07e0
jayram10125 <jayram.10125@gmail.com>
2024-04-05 17:52:46 +0530

    constructor

---
+++ OBJECT & CLASSES/constructor.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class student
+{
+    public :
+    student()   //constructor
+    {
+        cout<<"i am constructor \n";
+    }
+    void display();
+};
+void student :: display()
+{
+    cout<<"i am display";
+}
+int main()
+{
+    student s1;
+    s1. display();
+}
