aee6e27f4ca3577eabbf86343b9fc13a9199f4c5
jayram10125 <jayram.10125@gmail.com>
2024-04-18 10:06:09 +0530

    findinng diagonal elements

---
+++ ARRAY/2D ARRAY/printTheDiagonalElement.cpp
@@ -0,0 +1,40 @@
+//WAP to print the diagonal element of  an array
+#include<iostream>
+using namespace std;
+int  main()
+{
+    int size;
+    cout<<"enter the size of square matrix : ";
+    cin>>size;
+    int arr[size][size];
+    cout<<"enter the "<<size*size<<" elements of array : \n";
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"the elements of array :\n";
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"the diagonal elements are :\n";
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            if(i==j)
+            {
+            cout<<arr[i][j];
+            }
+        }
+        cout<<endl;
+    }
+
+}
